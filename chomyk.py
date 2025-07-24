0f8ed31436734524d4595357edee26087f8a30ab
kiepscy-archive <rudykr53@gmail.com>
2025-07-24 21:30:55 +0200

    Update chomyk.py

--- chomyk.py
+++ chomyk.py
@@ -91,6 +91,10 @@ class Chomyk:
         
     def cls(self):
         os.system('cls' if os.name=='nt' else 'clear')
+
+    def printline(self, line, text):
+        sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (line, 2, text))
+        sys.stdout.flush()
         
     def checkThreads(self):
         threadsInprogress = 0
